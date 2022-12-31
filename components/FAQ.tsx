53db8a0d6ec856a6aa4ab5ab48118f54f410974d
Paul Dolden <paul@dolden.co.uk>
2022-12-31 11:30:00 +0000

    remove logo

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -6,7 +6,6 @@ interface Props {
 }
 
 const FAQ = ({ data }: Props) => {
-    console.log(data);
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
